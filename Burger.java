65b457f344f65b586cbe9888db2ec21e4946cefe
Keith-McClelland <keithmcclelland5@gmail.com>
2025-05-13 08:47:29 -0400

    Added burger class

---
+++ Burger.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Burger here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Burger extends Actor
+{
+    
+    /**
+     * Act - do whatever the Burger wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int speed = 1;
+    public void act()
+    {
+        setLocation(getX(), getY() + speed);
+        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight()){
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+    public void getSpeed(int spd){
+        speed = spd;
+    }
+}
